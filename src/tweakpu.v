561862a53313bda7a75836375263569d97f5f087
Michael T. Mayers <michael@tweakoz.com>
2013-03-02 22:46:29 -0800

    cleanup

--- src/tweakpu.v
+++ src/tweakpu.v
@@ -35,9 +35,6 @@
 //    12 bits: registers
 //    12 bits: data
 
-
-
-
 //////////////////////////////////////////////////////////////////
 // instruction decoder
 //////////////////////////////////////////////////////////////////
@@ -62,8 +59,8 @@ module tweak_decoder(	input [31:0] OPCODE,		// incoming: composite instruction c
 	wire [23:0] data_16 = { OPCODE[23:8], 8'h00 };
 	wire [23:0] data_12 = { OPCODE[23:12], 12'h000 };
 
-	wire [1:0] iecode = OPCODE[31:30];
-	wire [5:0] iicode = OPCODE[29:24];
+	wire [1:0] format_code = OPCODE[31:30];
+	wire [5:0] instr_code = OPCODE[29:24];
 
 	reg [23:0] data_out;
 	reg [11:0] regs_out;
@@ -71,7 +68,7 @@ module tweak_decoder(	input [31:0] OPCODE,		// incoming: composite instruction c
 	//////////////////////////////////////////
 	always @*
 	begin
-	  case(iecode)
+	  case(format_code)
 	    2'b00: data_out = data_24;	// 0 operands
 	    2'b01: data_out = data_20;	// 1 operands
 	    2'b10: data_out = data_16;	// 2 operands
@@ -81,7 +78,7 @@ module tweak_decoder(	input [31:0] OPCODE,		// incoming: composite instruction c
 	//////////////////////////////////////////
 	always @*
 	begin
-	  case(iecode)
+	  case(format_code)
 	    2'b00: regs_out = regs_0;
 	    2'b01: regs_out = regs_4;
 	    2'b10: regs_out = regs_8;
@@ -91,8 +88,8 @@ module tweak_decoder(	input [31:0] OPCODE,		// incoming: composite instruction c
 	
 	//////////////////////////////////////////
 	
-	assign encoding[1:0] = iecode[1:0];		
-	assign alu_opcode[5:0] = iicode[5:0];		
+	assign encoding[1:0] = format_code[1:0];		
+	assign alu_opcode[5:0] = instr_code[5:0];		
 	assign immed_data[23:0] = data_out[23:0];		
 	assign reg_code[11:0] = regs_out[11:0];		
 	
@@ -102,7 +99,7 @@ endmodule
 // ALU
 //////////////////////////////////////////////////////////////////
 
-module tweak_alu(	input AluTrigger, input [5:0] alu_opcode, 
+module tweak_alu(	input [5:0] alu_opcode, 
 					input [31:0] arg_a, input [31:0] arg_b, 
 					output [31:0] result );
 
@@ -114,29 +111,25 @@ module tweak_alu(	input AluTrigger, input [5:0] alu_opcode,
 		alucode_and = 6'b000010,
 		alucode_or =  6'b000011,
 		alucode_xor = 6'b000100;
-
-	reg [31:0] res = 0;
 	
+	wire [31:0] res_mux;
+
 	wire [31:0] res_add = (arg_a+arg_b);
 	wire [31:0] res_sub = (arg_a-arg_b);
 	wire [31:0] res_and = (arg_a&arg_b);
 	wire [31:0] res_or = (arg_a|arg_b);
 	wire [31:0] res_xor = (arg_a^arg_b);
 
-	always @( posedge AluTrigger )
-	begin
-		case( alu_opcode )
-			alucode_add :	begin		res<=res_add;		end
-			alucode_sub :	begin		res<=res_sub;		end
-			alucode_and :	begin		res<=res_and;		end
-			alucode_or  :	begin		res<=res_or;		end
-			alucode_xor :	begin		res<=res_xor;		end
-			default :		begin		res<=arg_a;			end
-		endcase
-	end
-	
-	assign result = res;
-	
+	assign res_mux
+		= (alu_opcode==alucode_add) ? res_add
+		: (alu_opcode==alucode_sub) ? res_sub
+		: (alu_opcode==alucode_and) ? res_and
+		: (alu_opcode==alucode_xor) ? res_xor
+		: (alu_opcode==alucode_or) ? res_or
+		: arg_a;
+		
+	assign result[31:0] = res_mux[31:0];
+
 endmodule
 
 //////////////////////////////////////////////////////////////////
@@ -147,24 +140,40 @@ endmodule
 // Registers
 //////////////////////////////////////////////////////////////////
 
-module tweak_registers( input ls_trigger, input rW, input [3:0] address, input [31:0] inp, output [31:0] outp );
+module tweak_registers( input clear_trigger,
+						input l_trigger,
+						input s_trigger,
+						input [3:0] addra,
+						input [3:0] addrb,
+						input [31:0] inp,
+						output [31:0] outpa,
+						output [31:0] outpb );
 
 	reg [31:0] registers [0:15];
-	reg [31:0] outputbuffer;
-	
-	wire LoadTrigger = ls_trigger&(!rW);
-	wire StoreTrigger = ls_trigger&rW;
-	
-	always @( posedge StoreTrigger )
-		begin
-			registers[address] = inp; 
-		end
-	always @( posedge LoadTrigger )
-		begin
-			outputbuffer = registers[address]; 
-		end
+	reg [31:0] outbufa;
+	reg [31:0] outbufb;
 		
-	assign outp = outputbuffer;
+	always @( clear_trigger )
+		begin
+			registers[0] <= 0;
+			registers[1] <= 1;
+			registers[2] <= 2;
+			registers[3] <= 3;
+			registers[4] <= 4;
+			registers[5] <= 5;
+			registers[6] <= 6;
+		end
+	always @( posedge s_trigger )
+		begin
+			registers[addra] = inp; 
+		end
+	always @( posedge l_trigger )
+		begin
+		outbufa <= registers[addra];		
+		outbufb <= registers[addrb];		
+		end
+	assign outpa = outbufa;
+	assign outpb = outbufb;
 
 endmodule
 
@@ -172,7 +181,9 @@ endmodule
 // ROM
 //////////////////////////////////////////////////////////////////
 
-module tweak_rom( input read_ena, input [3:0] addr_in, output [31:0] data_out );
+module tweak_rom( input read_ena,
+				  input [3:0] addr_in,
+				  output [31:0] data_out );
  
 	parameter NUMWORDS = 8;
 	parameter WORDSIZE = 32;
@@ -203,14 +214,13 @@ module tweak_rom( input read_ena, input [3:0] addr_in, output [31:0] data_out );
  
  //////////////////////////////////////////////////////////////////
 
-module tweak_cpu (input CLK, input RESET, output op );
+module tweak_cpu( input CLK,
+				  input RESET,
+				  output op );
 
 	//////////////////////////////
 
-	reg [3:0] ADDRESS;
 	reg [31:0] composite_opcode;
-	reg [31:0] regobuf;
-	reg [31:0] regibuf;
 
 	//////////////////////////////
 	// sub clocks
@@ -234,19 +244,28 @@ module tweak_cpu (input CLK, input RESET, output op );
 	wire phase_D = clk_h4&phase_D_enable;
 
 	//////////////////////////////
-	// registers
+	// register file
 	//////////////////////////////
 
-	wire 		reg_ls_trigger;
-	wire [3:0]	reg_address;
-	wire 		reg_l_or_s;
-	wire [31:0] reg_input;
-	wire [31:0] reg_output;
+	wire 		reg_clear_trigger;
+	wire 		reg_l_trigger;
+	wire 		reg_s_trigger;
+	reg [31:0] reg_input;
+
+	wire [3:0]	reg_addra;
+	wire [3:0]	reg_addrb;
+	wire [31:0] reg_outpa;
+	wire [31:0] reg_outpb;
 	
 	//////////////////////////////
 
+	reg [3:0] rom_address;
 	wire [31:0] rom_data;
+
+	//////////////////////////////
+
 	wire [31:0] alu_result;
+	reg [31:0] alu_result_buf;
 
 	//////////////////////////////
 	// decoder output
@@ -257,6 +276,7 @@ module tweak_cpu (input CLK, input RESET, output op );
 	wire [23:0] dec_immed_data;
 	wire [11:0] dec_register_code;
 
+
 	//////////////////////////////
 	// boot up
 	//////////////////////////////
@@ -264,7 +284,7 @@ module tweak_cpu (input CLK, input RESET, output op );
 	initial begin
 		phase_C_enable = 0;
 		phase_D_enable = 0;
-		ADDRESS = 4'h0;
+		rom_address = 4'h0;
 		clk_h1 = 1;
 		clk_h2 = 1;
 		composite_opcode = 32'h00;
@@ -277,14 +297,6 @@ module tweak_cpu (input CLK, input RESET, output op );
 		begin
 		phase_C_enable <= 1;
 		clk_h1 <= ~ clk_h1;
-		ADDRESS <= RESET ? 0 : ADDRESS+1;
-		end
-
-	//////////////////////////////
-
-	always @( negedge CLK )
-		begin
-		composite_opcode = rom_data; // sample ROM, write to composite_opcode
 		end
 
 	//////////////////////////////
@@ -295,36 +307,48 @@ module tweak_cpu (input CLK, input RESET, output op );
 		end
 
 	//////////////////////////////
+	// PhaseA (ins fetch)
+	//////////////////////////////
 
 	always @ (posedge phase_A )
 		begin
+		composite_opcode = rom_data; // sample ROM, write to composite_opcode
 		end
 		
 	//////////////////////////////
+	// PhaseB (load)
+	//////////////////////////////
 
-	always @ (posedge phase_B )
-		begin
-		regibuf <= regobuf;
-		end
+	assign reg_clear_trigger = ! RESET;
+	assign reg_addra[3:0] = dec_register_code[3:0];
+	assign reg_addrb[3:0] = dec_register_code[7:4];
 
+	assign reg_l_trigger = phase_A&phase_B;// & (dec_encoding_fmt==1) & (dec_alu_opcode<2);
+
+	//always @ (posedge phase_B )
+	//	begin
+	//	end
+
+	//////////////////////////////
+	// PhaseC 
 	//////////////////////////////
 
 	always @( posedge phase_C )
 		begin
-		regobuf <= reg_output; // buffer the output register
 		phase_D_enable <= 1;
+		alu_result_buf <= alu_result;
+		reg_input <= alu_result;
 		end
 
 	//////////////////////////////
-	
-	always @( posedge phase_C )
-		begin
-		end
-
+	// PhaseD (store, ins addr ca)
 	//////////////////////////////
+
+	assign reg_s_trigger = phase_C&phase_D;// & (dec_encoding_fmt==1) & (dec_alu_opcode<2);
 
 	always @( posedge phase_D )
 		begin
+		rom_address <= rom_address+1;
 		end
 
 	//////////////////////////////
@@ -332,8 +356,7 @@ module tweak_cpu (input CLK, input RESET, output op );
 	//  todo : replace with load store controller
 	//////////////////////////////
 
-	assign reg_ls_trigger = phase_B & (dec_encoding_fmt==1) & (dec_alu_opcode<2);
-	assign reg_l_or_s = (dec_encoding_fmt==1);
+
 	assign reg_address = dec_register_code[3:0];
 
 	//////////////////////////////
@@ -342,13 +365,20 @@ module tweak_cpu (input CLK, input RESET, output op );
 
 	tweak_decoder	the_decoder( composite_opcode, dec_encoding_fmt, dec_alu_opcode, dec_immed_data, dec_register_code );
 
-	tweak_registers	the_regs( reg_ls_trigger, reg_l_or_s, reg_address, reg_input, reg_output );
+	tweak_registers	the_regs( reg_clear_trigger,
+							  reg_l_trigger,
+							  reg_s_trigger,
+							  reg_addra,
+							  reg_addrb,
+							  reg_input,
+							  reg_outpa,
+							  reg_outpb );
 
-	tweak_alu		the_alu(	phase_C, dec_alu_opcode, 
-								regibuf, regobuf, 
-								alu_result );
+	tweak_alu		the_alu( dec_alu_opcode, 
+							 reg_outpa, reg_outpb, 
+							 alu_result );
 						
-	tweak_rom 		the_rom( CLK, ADDRESS, rom_data );
+	tweak_rom 		the_rom( CLK, rom_address, rom_data );
 
 	//////////////////////////////
 	//
@@ -359,21 +389,14 @@ module tweak_cpu (input CLK, input RESET, output op );
 endmodule
 
 //////////////////////////////////////////////////////////////////
-
 module testbench;
-
-	///////////////////////////////
-
 	reg CLK = 0;
 	reg RESET = 1;
 	wire opo;
-
 	///////////////////////////////
 	tweak_cpu tpu ( CLK, RESET, opo );
 	///////////////////////////////
-
 	integer i;
-
 	initial begin
 //	$hello("yo",1);
 	$dumpfile("tweakpu.vcd");
@@ -382,9 +405,5 @@ module testbench;
 	for( i=0; i<100; i=i+1 )
 		CLK = #2 ~CLK;	
 	end
-
-
 endmodule
-
-
-/////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
